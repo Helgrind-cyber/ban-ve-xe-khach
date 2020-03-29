a6db0cf777e7c80e7f6fa19013b3738765ef62d2
helgrind-cyber <helgrindxxx@gmail.com>
2020-03-29 11:10:22 +0700

    add file register

--- public/_share/header.php
+++ public/_share/header.php
@@ -17,14 +17,6 @@
                     <li class="nav-item">
                         <a class="nav-link" href="<?php echo SEARCH_URL ?>">Danh sách vé</a>
                     </li>
-                    <li class="nav-item" style="<?php if ($loggedInUser !== null && $loggedInUser['role_id'] > 1) {
-                                                    echo 'display:inline-block';
-                                                } else {
-                                                    echo 'display:none';
-                                                } ?>">
-                        <a class="nav-link text-danger font-weight-bold text-uppercase" href="<?php echo ADMIN_URL . 'dashboard' ?>">Quản lý Trang Web</a>
-                    </li>
-
                     <?php if ($loggedInUser) : ?>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Hi, <?= $loggedInUser['name']; ?></a>
@@ -32,17 +24,22 @@
                                 <a class="dropdown-item" href="#">Thông tin cá nhân</a>
                                 <a class="dropdown-item" href="#">Đổi mật khẩu</a>
                                 <a class="dropdown-item" href="#">Thông tin vé xe</a>
+                                <a class="dropdown-item text-danger" style="<?php if ($loggedInUser !== null && $loggedInUser['role_id'] > 1) {
+                                                                                                            echo 'display:inline-block';
+                                                                                                        } else {
+                                                                                                            echo 'display:none';
+                                                                                                        } ?>" href="<?= ADMIN_URL . 'dashboard' ?>">Quản lý Trang Web</a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="<?php echo BASE_URL . './logout.php' ?>">Đăng xuất</a>
+                                <a class="dropdown-item" href="<?= BASE_URL . 'login/logout.php' ?>">Đăng xuất</a>
                             </div>
                         </li>
                     <?php else : ?>
                         <li class="nav-item">
-                            <a class="nav-link" href="login.php" title="">Đăng Nhập</a>
+                            <a class="nav-link" href="<?= BASE_URL.'login/login.php'?>" title="">Đăng Nhập</a>
                         </li>
                     <?php endif ?>
                     <li class="btn-cart nav-item btn btn-outline-primary">
-                        <a class="nav-link" href="<?php echo CART_URL ?>" title="">Giỏ Hàng <i class="fa fa-shopping-basket" aria-hidden="true"></i></a>
+                        <a class="nav-link" href="<?= CART_URL ?>" title="">Giỏ Hàng <i class="fa fa-shopping-basket" aria-hidden="true"></i></a>
                     </li>
                 </ul>
                 <!-- </div> -->

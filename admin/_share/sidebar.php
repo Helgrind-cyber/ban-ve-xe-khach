0f2c1195bff7056b17fdb760fe77c8099b023ff0
helgrind-cyber <helgrindxxx@gmail.com>
2020-04-03 16:35:25 +0700

    Change side bar

--- admin/_share/sidebar.php
+++ admin/_share/sidebar.php
@@ -2,7 +2,7 @@
     <!-- Brand Logo -->
     <a href="../../index.php" class="brand-link">
         <img src="<?= ADMIN_ASSET_URL ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">ICID Complex</span>
+        <span class="brand-text font-weight-light">Xe khách ND - HN</span>
     </a>
 
     <!-- Sidebar -->
@@ -10,7 +10,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="<?= BASE_URL . $_SESSION[AUTH]['avatar'] ?>" class="img-circle elevation-2" alt="User Image">
+                <img src="<?= PUBLIC_URL . 'images/default-image.jpg' ?>" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="#" class="d-block"><?= $_SESSION[AUTH]['name'] ?></a>
